b2d64f520fe0d8bba7616fe8fa93ac0dff5a24f6
syahirah <syahirah.suradi@gmail.com>
2019-12-15 22:15:42 +0800

    added logo, changed font family for body, added the quiz back to homepage

--- views/home.jsx
+++ views/home.jsx
@@ -17,7 +17,49 @@ class Home extends React.Component {
                       <button className="btn btn-explore btn-lg" id="exploreBtn">Explore</button>
 
                       <div id="qnBoard">
-                        <p id="firstQn">TEST TEST</p>
+                        <form method="POST" action="/recommend" id="form">
+                            <h5>How are you feeling now?</h5>
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="mood" id="m1" value="happy" defaultChecked/>
+                              <label class="form-check-label" for="m1">Happy</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="mood" id= "m2" value="sad"/>
+                              <label class="form-check-label" for="m2">Sad</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="mood" id= "m3" value="angry"/>
+                              <label class="form-check-label" for="m3">Angry</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="mood" id= "m4" value="meh"/>
+                              <label class="form-check-label" for="m4">Meh</label>
+                            </div>
+
+                            <br />
+                            <br />
+                            <h5>How much time can you spare?</h5>
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="time" id="t1" value="60" defaultChecked/>
+                              <label class="form-check-label" for="t1">Less than 1 hour</label>
+                            </div>
+
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="time" id="t2" value="120"/>
+                              <label class="form-check-label" for="t2">1 - 2 hours</label>
+                            </div>
+
+                            <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="time" id="t3" value="240"/>
+                              <label class="form-check-label" for="t3">More than 2 hours</label>
+                            </div>
+                            <br />
+                            <br />
+                            <button type= "submit" value= "submit" className="btn btn-submit">Submit</button>
+                        </form>
+
+
+
                       </div>
 
 
--- views/layout.jsx
+++ views/layout.jsx
@@ -7,11 +7,13 @@ class Layout extends React.Component {
         <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css?family=Lato|Oswald&display=swap" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="style.css"/>
         </head>
         <body>
             <nav className="navbar navbar-expand-md navbar-dark" style={{backgroundColor: '#640D14'}}>
-                <a href="/home" className="navbar-brand">showMe.</a>
+                <a href="/home"><img src="../images/logo.png" id="logo"/></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
                     <span className="navbar-toggler-icon"></span>
                 </button>
